bbbd16c2cb804e5255a4ec4061ce36b69a991e75
Fabian Wolter <github@fabian-wolter.de>
2021-09-10 00:42:35 +0200

    [pidcontroller] Add ability to limit the I-part
    Signed-off-by: Fabian Wolter <github@fabian-wolter.de>

--- bundles/org.openhab.automation.pidcontroller/src/main/java/org/openhab/automation/pidcontroller/internal/handler/PIDControllerTriggerHandler.java
+++ bundles/org.openhab.automation.pidcontroller/src/main/java/org/openhab/automation/pidcontroller/internal/handler/PIDControllerTriggerHandler.java
@@ -16,7 +16,6 @@ import static org.openhab.automation.pidcontroller.internal.PIDControllerConstan
 
 import java.math.BigDecimal;
 import java.util.Map;
-import java.util.Objects;
 import java.util.Optional;
 import java.util.Set;
 import java.util.concurrent.TimeUnit;
@@ -106,6 +105,8 @@ public class PIDControllerTriggerHandler extends BaseTriggerModuleHandler implem
         double kiAdjuster = getDoubleFromConfig(config, CONFIG_KI_GAIN);
         double kdAdjuster = getDoubleFromConfig(config, CONFIG_KD_GAIN);
         double kdTimeConstant = getDoubleFromConfig(config, CONFIG_KD_TIMECONSTANT);
+        double iMinValue = getDoubleFromConfig(config, CONFIG_I_MIN);
+        double iMaxValue = getDoubleFromConfig(config, CONFIG_I_MAX);
         pInspector = (String) config.get(P_INSPECTOR);
         iInspector = (String) config.get(I_INSPECTOR);
         dInspector = (String) config.get(D_INSPECTOR);
@@ -114,7 +115,7 @@ public class PIDControllerTriggerHandler extends BaseTriggerModuleHandler implem
         loopTimeMs = ((BigDecimal) requireNonNull(config.get(CONFIG_LOOP_TIME), CONFIG_LOOP_TIME + " is not set"))
                 .intValue();
 
-        controller = new PIDController(kpAdjuster, kiAdjuster, kdAdjuster, kdTimeConstant);
+        controller = new PIDController(kpAdjuster, kiAdjuster, kdAdjuster, kdTimeConstant, iMinValue, iMaxValue);
 
         eventFilter = event -> {
             String topic = event.getTopic();
@@ -144,7 +145,13 @@ public class PIDControllerTriggerHandler extends BaseTriggerModuleHandler implem
     }
 
     private double getDoubleFromConfig(Configuration config, String key) {
-        return ((BigDecimal) Objects.requireNonNull(config.get(key), key + " is not set")).doubleValue();
+        Object rawValue = config.get(key);
+
+        if (rawValue == null) {
+            return Double.NaN;
+        }
+
+        return ((BigDecimal) rawValue).doubleValue();
     }
 
     private void calculate() {
@@ -180,7 +187,8 @@ public class PIDControllerTriggerHandler extends BaseTriggerModuleHandler implem
 
     private void updateItem(@Nullable String itemName, double value) {
         if (itemName != null) {
-            eventPublisher.post(ItemEventFactory.createCommandEvent(itemName, new DecimalType(value)));
+            eventPublisher.post(ItemEventFactory.createStateEvent(itemName,
+                    Double.isFinite(value) ? new DecimalType(value) : UnDefType.UNDEF));
         }
     }
 
