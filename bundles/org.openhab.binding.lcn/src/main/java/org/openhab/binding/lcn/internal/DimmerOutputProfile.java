fc89f2a46249517acb512ad9b5de61c18bfbf53f
Fabian Wolter <github@fabian-wolter.de>
2021-12-11 20:46:23 +0100

    test5

--- bundles/org.openhab.binding.lcn/src/main/java/org/openhab/binding/lcn/internal/DimmerOutputProfile.java
+++ bundles/org.openhab.binding.lcn/src/main/java/org/openhab/binding/lcn/internal/DimmerOutputProfile.java
@@ -37,6 +37,7 @@ import org.slf4j.LoggerFactory;
 @NonNullByDefault
 public class DimmerOutputProfile implements StateProfile {
     private int test;
+
     private final Logger logger = LoggerFactory.getLogger(DimmerOutputProfile.class);
     /** The Profile's UID */
     static final ProfileTypeUID UID = new ProfileTypeUID(LcnBindingConstants.BINDING_ID, "output");
