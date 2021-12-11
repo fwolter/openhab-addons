38ecef8fe1d88428e992f73612ba94a06fbc2efd
Fabian Wolter <github@fabian-wolter.de>
2021-12-11 20:54:25 +0100

    test7

--- bundles/org.openhab.binding.lcn/src/main/java/org/openhab/binding/lcn/internal/DimmerOutputProfile.java
+++ bundles/org.openhab.binding.lcn/src/main/java/org/openhab/binding/lcn/internal/DimmerOutputProfile.java
@@ -52,7 +52,7 @@ public class DimmerOutputProfile implements StateProfile {
         Optional<Object> ramp = getConfig(profileContext, "ramp");
         Optional<Object> allOutputs = getConfig(profileContext, "controlAllOutputs");
         Optional<Object> outputs12 = getConfig(profileContext, "controlOutputs12");
-        
+
         if (ramp == allOutputs) {
             rampMs = 0;
             // SAT error
